68d1a82a058111df059456b1e61d90cc7b96c047
pacco788 <49822144+pacco788@users.noreply.github.com>
2023-10-15 11:37:34 +0200

    Create tvheadend1.rb

---
+++ tvheadend1.rb
@@ -0,0 +1,37 @@
+require "formula"
+
+class Tvheadend < Formula
+  homepage "http://www.tvheadend.org"
+  head 'https://github.com/tvheadend/tvheadend.git'
+  
+  depends_on 'pkg-config' => :build
+  depends_on 'uriparser'
+  
+  def install
+    args = ["--prefix=#{prefix}"]
+    system "./configure", *args
+    system "make install"
+  end
+  
+  def post_install
+    unless File.exist? "#{var}/tvheadend"
+      mkdir_p "#{var}/tvheadend/accesscontrol"
+      (var/'tvheadend/accesscontrol/1').write <<-EOS.undent
+      {
+        "enabled": 1,
+        "username": "*",
+        "password": "*",
+        "comment": "Default access entry",
+        "prefix": "0.0.0.0/0,::/0",
+        "streaming": 1,
+        "dvr": 1,
+        "dvrallcfg": 1,
+        "webui": 1,
+        "admin": 1,
+        "tag_only": 0,
+        "id": "1"
+      }
+      EOS
+    end
+  end
+end
